72992f6e03d60ed7da5b1110c3e5ee38bc985cb8
abrance <1103098607@qq.com>
2020-06-21 09:26:45 +0800

    add elpa.win10

---
+++ elpa.win10/helm-core-3.6.2/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "3.6.2" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "41242faebb5c16c23fe7e831d4bb2cb3294e074d" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
