07419dba28f67af16feb34199b403883df7f5623
abrance <1103098607@qq.com>
2021-07-28 17:11:07 +0800

    update tool bar hide

--- init.el
+++ init.el
@@ -1,10 +1,11 @@
 ;; 不使用 lisp 语言版本。
 
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
-(tool-bar-mode -1)
+(tool-bar-mode 0)
 
 ;; 关闭文件滑动控件
-(scroll-bar-mode -1)
+;; (scroll-bar-mode -1)
+(menu-bar-no-scroll-bar)
 
 ;; 显示行号
 ;; (global-linum-mode 1)
