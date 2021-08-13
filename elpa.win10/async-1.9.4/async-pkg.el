27496f2a818506dbab6fbf987bd8c1641e00493a
abrance <your_email>
2021-08-13 14:42:11 +0800

    win10: linux c; no save bak; font; desktop

--- elpa.win10/async-1.9.4/async-pkg.el
+++ elpa.win10/async-1.9.4/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "1.9.4" "Asynchronous processing in Emacs" 'nil :commit "67c369555de998eaabd60056dead038c6c50b8fd" :keywords
+(define-package "async" "1.9.4" "Asynchronous processing in Emacs" 'nil :keywords
   '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:
--- elpa.win10/dash-2.17.0/dash-pkg.el
+++ elpa.win10/dash-2.17.0/dash-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dash" "2.17.0" "A modern list library for Emacs" 'nil :commit "721436b04da4e2795387cb48a98ac6de37ece0fd" :keywords
+(define-package "dash" "2.17.0" "A modern list library for Emacs" 'nil :keywords
   '("lists")
   :authors
   '(("Magnar Sveen" . "magnars@gmail.com"))
